ff02417e5686eb7b8c733875edacf10967cc9f6a
meick <michael@meick.biz>
2025-07-26 21:14:40 +0200

    support halt, noop, out

--- src/machine.py
+++ src/machine.py
@@ -6,7 +6,10 @@ class Machine:
 
     def __init__(self):
         self.__reset()
+        self.memory = [0]*Machine.MEM_SIZE
         self.dispatch = [None]*22
+        self.dispatch[19] = self.__out
+        self.dispatch[21] = self.__noop
 
     def load(self, data):
         lend = len(data)
@@ -19,10 +22,27 @@ class Machine:
             if opcode == 0:
                 return
             instr = self.dispatch[opcode]
-            instr()
+            self.pc += instr()
 
     def __reset(self):
         self.registers = [0]*8
-        self.memory = [0]*Machine.MEM_SIZE
         self.stack = []
         self.pc = 0
+        self.term_out = ""
+
+    def __out(self):
+        ch = self.__literal_or_reg(1)
+        self.term_out += chr(ch)
+        return 2
+
+    def __noop(self):
+        return 1
+
+    def __literal_or_reg(self, offset):
+        arg = self.memory[self.pc + offset]
+        if arg < 32768:
+            return arg
+        elif arg < 32776:
+            return self.registers[arg - 32768]
+        else:
+            raise Exception(f"Invalid argument {arg}")
--- src/test_instr.py
+++ src/test_instr.py
@@ -9,5 +9,18 @@ class InstructionTest(unittest.TestCase):
         m.run()
         self.assertEqual(m.pc, 0)
 
+    def test_noop(self):
+        m = Machine()
+        m.load([21,0])
+        m.run()
+        self.assertEqual(m.pc, 1)
+
+    def test_out(self):
+        m = Machine()
+        m.load([19,ord("$"),0])
+        m.run()
+        self.assertEqual(m.pc, 2)
+        self.assertEqual(m.term_out,"$")
+
 if __name__ == '__main__':
     unittest.main()

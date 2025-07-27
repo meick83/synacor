468326c170a6fb99a804f74a1a4a9e72b251efe1
meick <michael@meick.biz>
2025-07-27 21:57:29 +0200

    support more instructions

--- src/machine.py
+++ src/machine.py
@@ -14,13 +14,21 @@ class InstructionDecoder:
         self.dispatch_obj = dispatch_obj
         self.instr = [None]*22
         self.instr[ 0] = self.Instr(dispatch_obj.instr_exit)
-        self.instr[ 1] = self.Instr(dispatch_obj.instr_set, self.__O, self.__I)
-        self.instr[ 4] = self.Instr(dispatch_obj.instr_eq , self.__O, self.__I, self.__I)
-        self.instr[ 6] = self.Instr(dispatch_obj.instr_jmp, self.__I)
-        self.instr[ 7] = self.Instr(dispatch_obj.instr_jt , self.__I, self.__I)
-        self.instr[ 8] = self.Instr(dispatch_obj.instr_jf , self.__I, self.__I)
-        self.instr[ 9] = self.Instr(dispatch_obj.instr_add, self.__O, self.__I, self.__I)
-        self.instr[19] = self.Instr(dispatch_obj.instr_out, self.__I)
+        self.instr[ 1] = self.Instr(dispatch_obj.instr_set,  self.__O, self.__I)
+        self.instr[ 2] = self.Instr(dispatch_obj.instr_push, self.__I)
+        self.instr[ 3] = self.Instr(dispatch_obj.instr_pop,  self.__O)
+        self.instr[ 4] = self.Instr(dispatch_obj.instr_eq ,  self.__O, self.__I, self.__I)
+        self.instr[ 5] = self.Instr(dispatch_obj.instr_gt ,  self.__O, self.__I, self.__I)
+        self.instr[ 6] = self.Instr(dispatch_obj.instr_jmp,  self.__I)
+        self.instr[ 7] = self.Instr(dispatch_obj.instr_jt ,  self.__I, self.__I)
+        self.instr[ 8] = self.Instr(dispatch_obj.instr_jf ,  self.__I, self.__I)
+        self.instr[ 9] = self.Instr(dispatch_obj.instr_add,  self.__O, self.__I, self.__I)
+        self.instr[10] = self.Instr(dispatch_obj.instr_mult, self.__O, self.__I, self.__I)
+        self.instr[11] = self.Instr(dispatch_obj.instr_mod,  self.__O, self.__I, self.__I)
+        self.instr[12] = self.Instr(dispatch_obj.instr_and,  self.__O, self.__I, self.__I)
+        self.instr[13] = self.Instr(dispatch_obj.instr_or,   self.__O, self.__I, self.__I)
+        self.instr[14] = self.Instr(dispatch_obj.instr_not,  self.__O, self.__I)
+        self.instr[19] = self.Instr(dispatch_obj.instr_out,  self.__I)
         self.instr[21] = self.Instr(dispatch_obj.instr_noop)
 
     def decode(self, mem, pos):
@@ -84,9 +92,12 @@ class Machine:
 
 
     def __init__(self):
-        self.__reset()
         self.registers = list(map(self.Register,range(8)))
         self.memory = [0]*Machine.MEM_SIZE
+        self.stack = []
+        self.pc = 0
+        self.term_out = ""
+
         self.instr_exit = None
         self.get_literal = Machine.Literal
         self.get_register = self.registers.__getitem__
@@ -97,7 +108,6 @@ class Machine:
         self.memory[0:lend] = data[:]
 
     def run(self):
-        self.__reset()
         while True:
             dispatch, self.next_pc, args = self.decoder.decode(self.memory, self.pc)
             if dispatch is None:
@@ -105,16 +115,23 @@ class Machine:
             dispatch(*args)
             self.pc = self.next_pc
 
-    def __reset(self):
-        self.stack = []
-        self.pc = 0
-        self.term_out = ""
-
     def instr_set(self, a, b):
         a.set(b.get())
 
+    def instr_push(self, a):
+        self.stack.append(a.get())
+
+    def instr_pop(self, a):
+        a.set(self.stack.pop())
+
     def instr_eq(self, a, b, c):
         if b.get() == c.get():
+            a.set(1)
+        else:
+            a.set(0)
+
+    def instr_gt(self, a, b, c):
+        if b.get() > c.get():
             a.set(1)
         else:
             a.set(0)
@@ -133,6 +150,26 @@ class Machine:
 
     def instr_add(self, a, b, c):
         av = (b.get()+c.get())%Machine.MODULO
+        a.set(av)
+
+    def instr_mult(self, a, b, c):
+        av = (b.get()*c.get())%Machine.MODULO
+        a.set(av)
+
+    def instr_mod(self, a, b, c):
+        av = b.get()%c.get()
+        a.set(av)
+
+    def instr_and(self, a, b, c):
+        av = b.get()&c.get()
+        a.set(av)
+    
+    def instr_or(self, a, b, c):
+        av = b.get()|c.get()
+        a.set(av)
+
+    def instr_not(self, a, b):
+        av = ~b.get()&0x7FFF
         a.set(av)
         
 
--- src/test_instr.py
+++ src/test_instr.py
@@ -10,6 +10,22 @@ class InstructionTest(unittest.TestCase):
         self.assertEqual(m.pc, 3)
         self.assertEqual(m.registers[0].get(), 1234)
 
+    def test_push(self):
+        m = Machine()
+        m.load([2, 1234])
+        m.run()
+        self.assertEqual(m.pc, 2)
+        self.assertListEqual(m.stack,[1234])
+
+    def test_pop(self):
+        m = Machine()
+        m.load([3, 32768])
+        m.stack = [1234]
+        m.run()
+        self.assertEqual(m.pc, 2)
+        self.assertListEqual(m.stack,[])
+        self.assertEqual(m.registers[0].get(), 1234)
+
     def test_eq(self):
         m = Machine()
         m.load([4,32768,1,1])
@@ -18,6 +34,18 @@ class InstructionTest(unittest.TestCase):
         self.assertEqual(m.registers[0].get(), 1)
         m = Machine()
         m.load([4,32768,1,0])
+        m.run()
+        self.assertEqual(m.pc, 4)
+        self.assertEqual(m.registers[0].get(), 0)
+
+    def test_gt(self):
+        m = Machine()
+        m.load([5,32768,1,0])
+        m.run()
+        self.assertEqual(m.pc, 4)
+        self.assertEqual(m.registers[0].get(), 1)
+        m = Machine()
+        m.load([5,32768,1,2])
         m.run()
         self.assertEqual(m.pc, 4)
         self.assertEqual(m.registers[0].get(), 0)
@@ -54,6 +82,41 @@ class InstructionTest(unittest.TestCase):
         m.run()
         self.assertEqual(m.pc, 4)
         self.assertEqual(m.registers[0].get(), 12)
+
+    def test_mult(self):
+        m = Machine()
+        m.load([10,32768,3,4,0])
+        m.run()
+        self.assertEqual(m.pc, 4)
+        self.assertEqual(m.registers[0].get(), 12)
+
+    def test_mod(self):
+        m = Machine()
+        m.load([11,32768,4,3,0])
+        m.run()
+        self.assertEqual(m.pc, 4)
+        self.assertEqual(m.registers[0].get(), 1)
+
+    def test_and(self):
+        m = Machine()
+        m.load([12,32768,4,7,0])
+        m.run()
+        self.assertEqual(m.pc, 4)
+        self.assertEqual(m.registers[0].get(), 4)
+
+    def test_or(self):
+        m = Machine()
+        m.load([13,32768,4,3,0])
+        m.run()
+        self.assertEqual(m.pc, 4)
+        self.assertEqual(m.registers[0].get(), 7)
+
+    def test_not(self):
+        m = Machine()
+        m.load([14,32768,1,0])
+        m.run()
+        self.assertEqual(m.pc, 3)
+        self.assertEqual(m.registers[0].get(), 0x7FFE)
 
     def test_halt(self):
         m = Machine()
